9bbb3b7b3fdf87e8082310b742b50f75e598f884
ByongE <kbh25963@gmail.com>
2022-05-08 23:09:43 +0900

    202200508 Test sort

--- hello/src/MyExSpace.java
+++ hello/src/MyExSpace.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import lombok.Getter;
 import lombok.Setter;
 @Getter
@@ -6,12 +8,16 @@ class MyExSpace{
 	//	MyExSpace(){}
 	public static void main(String[] args) {
 	
-		int a = 10;
+		//정렬
+		String[] arr = {"Ab","ab","56","34","12"};
+		System.out.println(Arrays.toString(arr));	
 		
-	}
-	static void test12() { 
-		int abc = 5;
-		System.out.println("abcbcbc");
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));	
+			
+		
+	
+		
 	}
 }
 
