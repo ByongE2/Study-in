3c000c0ca69a4823c7ae72bb7a023bb699bee585
ByongE <kbh25963@gmail.com>
2022-06-02 17:37:14 +0900

    Study DAO,DTO,VO

---
+++ web_study_09/src/main/java/com/saeyan/dao/MemberDAO.java
@@ -0,0 +1,168 @@
+package com.saeyan.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.saeyan.dto.MemberVO;
+
+public class MemberDAO {
+	private MemberDAO() {}
+	
+	//싱글톤 패턴으로 객체 생성
+	private static MemberDAO instance = new MemberDAO();
+	
+	public static MemberDAO getInstance() {
+		return instance;
+	}
+	public Connection getConnection() throws Exception {
+		Context initContext = new InitialContext();
+		Context envContext  = (Context)initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+		Connection conn = ds.getConnection();
+		return conn;
+		
+	}
+	public int userCheck(String userid, String pwd) {
+	
+		int result = -1;
+		String sql = "select pwd from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				if(rs.getString("pwd").equals(pwd) && rs.getString("pwd") != null) {
+					result = 1; // 일치하면.
+				}else {
+					result = 0; // 일치안하면 
+				}
+			}else {
+				result = -1; // 기본회원이 아니다.
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return result;
+	}
+	public MemberVO getMember(String userid) {
+		MemberVO mVo = null;
+		String sql = "select * from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				mVo = new MemberVO();
+				mVo.setName(rs.getString("name"));
+				mVo.setUserid(rs.getString("userid"));
+				mVo.setPwd(rs.getString("Pwd"));
+				mVo.setEmail(rs.getString("email"));
+				mVo.setPhone(rs.getString("phone"));
+				mVo.setAdmin(rs.getInt("admin"));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return mVo;
+	}//getMember
+	public int confirmID(String userid) {
+		int result = -1;
+		String sql = "select userid from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery(); // 반환값이 ResultSet임. 컬럼이 없을 때 null을 반환한다. > 그래서 result 임의로 정해주는듯?
+			if(rs.next()) {
+				result = 1; 
+			}else {
+				result = -1;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		return result;
+	}//confirmID
+	public int insertMember(MemberVO mVo) {
+		int result = -1;
+		String sql = "insert into member values(?,?,?,?,?,?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mVo.getName());
+			pstmt.setString(2, mVo.getUserid());
+			pstmt.setString(3, mVo.getPwd());
+			pstmt.setString(4, mVo.getEmail());
+			pstmt.setString(5, mVo.getPhone());
+			pstmt.setInt(6, mVo.getAdmin());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if( pstmt != null) pstmt.close();
+				if( conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}//insertMember
+	
+}//end of MemberDAO
