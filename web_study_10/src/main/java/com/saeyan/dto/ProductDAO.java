5f7351960be05c70dc4fe454008bef4064fb8d49
ByongE <kbh25963@gmail.com>
2022-06-13 20:43:31 +0900

    Study JusoTest&FileUploadBoard as jsp

---
+++ web_study_10/src/main/java/com/saeyan/dto/ProductDAO.java
@@ -0,0 +1,126 @@
+package com.saeyan.dto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import util.DBManager;
+
+public class ProductDAO {
+
+	private ProductDAO() {}
+	//싱글톤 객체 생성
+	private static ProductDAO instance = new ProductDAO();
+	
+	public static ProductDAO getInstance() {
+		return instance;
+	}
+	
+	//c Read u d
+	public List<ProductVO> selectAllProducts(){
+		//최근 등록한 상품 먼저 출력하기
+		String sql = "select * from product order by code desc";
+		List<ProductVO> list = new ArrayList<ProductVO>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				ProductVO pVo = new ProductVO();
+				pVo.setCode(rs.getInt(1));
+				pVo.setName(rs.getString(2));
+				pVo.setPrice(rs.getInt(3));
+				pVo.setPictureUrl(rs.getString("PictureUrl"));
+				pVo.setDescription(rs.getString(5));
+				list.add(pVo);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.closeConnection(conn, pstmt, rs);
+		}
+		return list;
+	}//selectAllProducts
+	
+	public int insertProduct(ProductVO pVo) {
+		int result = -1;
+		String sql = "insert into product values(product_seq.nextval, ?, ?, ?, ?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, pVo.getName());
+			pstmt.setInt(2, pVo.getPrice());
+			pstmt.setString(3, pVo.getPictureUrl());
+			pstmt.setString(4, pVo.getDescription());
+			result = pstmt.executeUpdate();
+			System.out.println("result : " + result);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.closeConnection(conn, pstmt);
+		}
+		return result;
+	}//insertProduct
+	
+	public ProductVO selectProductByCode(String code) {
+		
+		String sql = "select * from product where code=?";
+		ProductVO pVo = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, code);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				pVo = new ProductVO();
+				pVo.setCode(rs.getInt("code"));
+				pVo.setName(rs.getString(2));
+				pVo.setPrice(rs.getInt(3));
+				pVo.setPictureUrl(rs.getString(4));
+				pVo.setDescription(rs.getString(5));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.closeConnection(conn, pstmt, rs);
+		}
+		
+		return pVo;
+	}//selectProductByCode
+	
+	public void updateProduct(ProductVO pVo) {
+		String sql = "update product set name=?, price=?, pictureUrl=?, description=? where code=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, pVo.getName());
+			pstmt.setInt(2, pVo.getPrice());
+			pstmt.setString(3, pVo.getPictureUrl());
+			pstmt.setString(4, pVo.getDescription());
+			pstmt.setInt(5, pVo.getCode());
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.closeConnection(conn, pstmt);
+		}
+	}//updateProduct
+	
+	
+}
